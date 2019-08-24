2488e30181b66bebc268ce4fa6460120c43750d1
Erik Borella <erikborella@gmail.com>
2019-08-24 09:43:28 -0300

    added login route

--- src/router.js
+++ src/router.js
@@ -18,6 +18,10 @@ export default new Router({
       path: '/login',
       name: 'Login',
       component: Login
+    },
+    {
+      path: '/loginRedirect',
+      redirect: 'login'
     }
   ]
 })
